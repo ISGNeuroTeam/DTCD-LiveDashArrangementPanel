b4048debced5cae57abba46bde0e8372f958a582
sbelikov98 <sbelikov@isgneuro.com>
2021-05-28 14:35:10 +0300

    Init project

---
+++ DTCD-LiveDashArrangementPanel/src/Plugin.Meta.js
@@ -0,0 +1,8 @@
+export default {
+  type: 'extension',
+  target: 'LiveDashPanel',
+  name: 'LiveDashArrangementPanel',
+  title: 'Панель расположений графа',
+  version: '0.1.0',
+  withDependencies: true,
+};
